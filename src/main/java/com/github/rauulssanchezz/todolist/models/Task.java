a37b1d79006c6d8de4ed976372e6608a0aa7b36e
rauulssanchezz <rauulssanchezz@gmail.com>
2025-08-31 17:38:39 +0200

    add constructor

--- src/main/java/com/github/rauulssanchezz/todolist/models/Task.java
+++ src/main/java/com/github/rauulssanchezz/todolist/models/Task.java
@@ -20,6 +20,12 @@ public class Task {
 
     private String title;
     private String description;
-    private boolean completed;
+    private Boolean completed;
+
+    public Task(String title, String description, Boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
 
 }
