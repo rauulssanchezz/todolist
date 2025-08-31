96e7bfc65b932619e999e36b563e0dd54dc7dc17
rauulssanchezz <rauulssanchezz@gmail.com>
2025-08-31 13:35:15 +0200

    controller

---
+++ src/main/java/com/github/rauulssanchezz/todolist/controller/TaskController.java
@@ -0,0 +1,63 @@
+package com.github.rauulssanchezz.todolist.controller;
+
+import java.util.List;
+import java.util.Optional;
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.rauulssanchezz.todolist.models.Task;
+import com.github.rauulssanchezz.todolist.services.TaskService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("")
+    public List<Task> getall() {
+        return taskService.getAllTaks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getById(@Valid @PathVariable Long id) {
+        Task task = taskService.getTaskById(id);
+        return ResponseEntity.ok(task);
+    }
+    
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@Valid @RequestBody Task task) {        
+        return taskService.createTask(task);
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Optional<Task>> updateTask(@Valid @PathVariable Long id, @RequestBody Task task) {
+        Optional<Task> updatedTask = taskService.updateTask(id, task);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@Valid @PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+
+}
