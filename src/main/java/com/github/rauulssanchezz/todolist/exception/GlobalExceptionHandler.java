c73ce741ff2816284f9bed765b3ec508b9523620
rauulssanchezz <rauulssanchezz@gmail.com>
2025-08-31 13:35:37 +0200

    globalexceptionhanlder

---
+++ src/main/java/com/github/rauulssanchezz/todolist/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.github.rauulssanchezz.todolist.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
